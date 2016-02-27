107a0c1b4f6fc85a7ed3be648d0c2a74aa2b10e4
saschoar <huth.sascha@gmail.com>
2016-02-26 22:34:17 -0500

    adapterchanges

--- app/src/main/java/com/saschahuth/brewy/ui/adapter/ItemAdapter.kt
+++ app/src/main/java/com/saschahuth/brewy/ui/adapter/ItemAdapter.kt
@@ -25,32 +25,32 @@ class ItemAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHold
     }
 
     override fun getItemCount(): Int {
-        return if (list == null) 0 else list.size
+        return list.size
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder?, position: Int) {
-        val location: Location? = list?.get(position)
+        val location: Location? = list[position]
         if (location != null) {
             (viewHolder?.itemView as LocationItemView).bind(location)
         }
     }
 
     fun add(toAdd: Location) {
-        list?.add(toAdd)
+        list.add(toAdd)
         notifyDataSetChanged()
     }
 
     fun addAll(toAdd: List<Location>) {
-        list?.addAll(toAdd)
+        list.addAll(toAdd)
         notifyDataSetChanged()
     }
 
     fun findById(id: String): Location? {
         try {
-            val count = list?.size ?: 0
+            val count = list.size
             for (i in 0..count) {
-                val location = list?.get(i)
-                if (location?.id == id) return location
+                val location = list[i]
+                if (location.id == id) return location
             }
         } catch(e: IndexOutOfBoundsException) {
             e.printStackTrace()
@@ -59,13 +59,13 @@ class ItemAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHold
     }
 
     fun sortByName() {
-        list?.sortBy {
+        list.sortBy {
             it.brewery?.name?.toLowerCase()
         }
     }
 
     fun sortByDistance() {
-        list?.sortBy {
+        list.sortBy {
             it.distanceTo(40.024925, -83.0038657)
         }
     }
